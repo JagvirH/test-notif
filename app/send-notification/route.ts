8333838d71c55427427e55a48b9d6e53367e9df3
JagvirH <jheer02@gmail.com>
2024-11-16 01:34:14 +0000

    e

--- app/send-notification/route.ts
+++ app/send-notification/route.ts
@@ -1,14 +1,35 @@
-import admin from "firebase-admin";
+import admin, { ServiceAccount } from "firebase-admin";
 import { Message } from "firebase-admin/messaging";
 import { NextRequest, NextResponse } from "next/server";
+import 'dotenv/config';
+
+// Utility function to fetch environment variables with error checking
+function getEnvVar(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not defined`);
+  }
+  return value;
+}
 
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
-  const serviceAccount = require("@/service_key.json");
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+  const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Ensure proper newline format
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  };
+
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    });
+  } catch (error) {
+    console.error("Firebase initialization error:", error);
+  }
 }
+
+
 
 export async function POST(request: NextRequest) {
   const { token, title, message, link } = await request.json();
@@ -29,7 +50,7 @@ export async function POST(request: NextRequest) {
   try {
     await admin.messaging().send(payload);
 
-    return NextResponse.json({ success: true, message: "Notification sent yAY!" });
+    return NextResponse.json({ success: true, message: "Notification sent!" });
   } catch (error) {
     return NextResponse.json({ success: false, error });
   }
